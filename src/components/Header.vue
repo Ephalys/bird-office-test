e63dfb34a16d1cdf661b2c9607c40e1b357aaa27
Ephalys <ephalys@live.fr>
2021-12-01 20:57:31 +0100

    add style header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,12 @@
       <img src="../assets/logo.svg" alt="logo" />
     </a>
     <div class="nav-cart">
-          <img src="../assets/icons/cart.svg" alt="cart">
-          <span>Shopping Cart</span>
-          <div>
-              {{cart_items}}
-          </div>
+      <img src="../assets/icons/cart.svg" alt="cart" />
+      <span class="nav-cart-label">Shopping Cart</span>
+      <div class="nav-cart-count">
+        {{ cart_items }}
       </div>
+    </div>
   </nav>
 </template>
 
@@ -17,10 +17,10 @@
 export default {
   name: "Header",
   data() {
-      return {
-          cart_items: 0,
-      }
-  }
+    return {
+      cart_items: 0,
+    };
+  },
 };
 </script>
 
@@ -32,9 +32,27 @@ export default {
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.15);
 
   &-cart {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+    padding-left: 15px;
+    border-left: 1px solid rgba(254, 189, 23, 0.1);
+
+    &-label {
+      font-size: 12px;
+      color: $light-grey;
+    }
+    &-count {
       display: flex;
       align-items: center;
       justify-content: center;
+      color: white;
+      background-color: $primary;
+      border-radius: 50%;
+      min-height: 36px;
+      min-width: 36px;
+    }
   }
 }
 </style>
